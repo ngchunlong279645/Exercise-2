2f7be21999707a053af5d4cb4b59ae1be7a457fc
long0618 <long0618@LAPTOP-D6N12UJA>
2021-04-05 15:51:08 +0800

    first-commit

---
+++ Main/src/PhoneProfile/Main.java
@@ -0,0 +1,43 @@
+package PhoneProfile;
+
+public class Main {
+
+	public static void main(String[] args) {
+		phone huawei = new phone();  //create new object from class phone name as huawei.
+		phone vivo = new phone();	//create second object from class phone name as vivo.
+		phone samsung = new phone(); //create third object from class phone name as samsung.
+		phone sony = new phone();	//create forth object from class phone name as sony.
+		phone asus = new phone();	//create fifth object from class phone name as asus.
+		
+		System.out.println("VIVO SPECIFICATION");	
+		vivo.printSpec("V10","RED",399,55.6,'Y');
+		vivo.convert(399, "Thailand");
+		vivo.payment(399, 6);
+		vivo.totalPrice(399, 5);	
+		
+		System.out.println("HUAWEI SPECIFICATION");	
+		huawei.printSpec("Nova3", "BLACK", 299, 49.5,'N');
+		huawei.convert(299, "USA");
+		huawei.payment(299, 5);
+		huawei.totalPrice(299, 2);
+		
+		System.out.println("SAMSUNG SPECIFICATION");	
+		samsung.printSpec("NOTE10","PINK",999,70.8,'N');
+		samsung.convert(999, "China");
+		samsung.payment(999, 9);
+		samsung.totalPrice(999, 3);
+		
+		System.out.println("SONY SPECIFICATION");	
+		sony.printSpec("Xperia 6","GREY",899,66.8,'Y');
+		sony.convert(899, "Japan");
+		sony.payment(899, 8);
+		sony.totalPrice(899, 6);
+		
+		System.out.println("ASUS SPECIFICATION");	
+		asus.printSpec("ROG 5","WHITE",599,95.2,'Y');	
+		asus.convert(599, "USA");
+		asus.payment(599, 3);
+		asus.totalPrice(599, 4);
+	}
+}
+
---
+++ Main/src/PhoneProfile/phone.java
@@ -0,0 +1,55 @@
+package PhoneProfile;
+
+public class phone {
+	// declare all type of variable and variable name
+	String brand;
+	int price,month,quantity;
+	String color,country;
+	double weight;
+	char fingerprint;
+	
+	//method print specification
+	void printSpec(String b,String c,int p,double w,char f) {
+		System.out.println("Brand \t\t\t: "+b);
+		System.out.println("Price \t\t\t: RM "+p);
+		System.out.println("Color \t\t\t: "+c);
+		System.out.println("Weight \t\t\t: "+w+" gram");
+		System.out.println("Fingerprint \t\t: "+f);	
+	}
+	
+	//method convert malaysia price to other country price 
+	void convert(int price,String country) {
+		double output=0;
+		String unit=null;
+			if(country.equals("USA")) {
+				output=price*0.24;
+				unit = "Dollar";
+			}else if(country.equals("Japan")) {
+				output=price*26.71;
+				unit = "Yen";
+			}else if(country.equals("China")) {
+				output=price*1.59;
+				unit = "RMB";
+			}else if(country.equals("Thailand")) {
+				output=price*7.58;
+				unit = "Baht";
+		}
+			System.out.printf("Price in %-14s : %.2f %s%n",country,output,unit);
+	}
+
+	// method calculate price to be paid per how many month
+	void payment(int price,int month) {
+		double pay = (double) price/month;
+		System.out.printf("Payment per %d months    : RM %.2f%n",month,pay);	
+	}
+	
+	// method calculate total price
+	void totalPrice(int price,int quantity) {
+		int total =price * quantity;
+		System.out.println("Total price of "+quantity+" phone  : RM "+total);
+		System.out.println("=====================================");
+	}
+}// end class
+
+
+
