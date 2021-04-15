d654569967536a6c93f512c5459de6b9b53b96f7
long0618 <long0618@LAPTOP-D6N12UJA>
2021-04-15 13:56:03 +0800

    FIRST-COMMIT

--- Main/src/QUIZ1/Main.java
+++ Main/src/QUIZ1/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Car Proton = new Car();
+		Car BMW = new Car();
 		
 		Proton.setModel();
 		Proton.setPrice();
 		Proton.setNumberOfSeat();
 		Proton.setEngineType();
 		Proton.setQuantity();
-
+		
+		
+		
 		System.out.println("=========================");
 		System.out.println(">>>>CAR INFORMATION<<<<<");
 		System.out.println("=========================");
@@ -20,6 +23,22 @@ public class Main {
 		System.out.println("Engine Type\t\t\t: "+Proton.getEngineType());
 		System.out.println("Quantity\t\t\t: "+Proton.getQuantity());
 		System.out.println("Total price\t\t\t: "+Proton.getTotalPrice());
+		
+		BMW.setModel();
+		BMW.setPrice();
+		BMW.setNumberOfSeat();
+		BMW.setEngineType();
+		BMW.setQuantity();
+		
+		System.out.println("=========================");
+		System.out.println(">>>>CAR INFORMATION<<<<<");
+		System.out.println("=========================");
+		System.out.println("Model\t\t\t\t: "+BMW.getModel());
+		System.out.println("Price\t\t\t\t: "+BMW.getPrice());
+		System.out.println("Number of seat\t\t\t: "+BMW.getNumberOfSeat());
+		System.out.println("Engine Type\t\t\t: "+BMW.getEngineType());
+		System.out.println("Quantity\t\t\t: "+BMW.getQuantity());
+		System.out.println("Total price\t\t\t: "+BMW.getTotalPrice());
 	}
 
 }
